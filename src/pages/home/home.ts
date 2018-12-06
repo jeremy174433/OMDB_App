09bcc68958bad2b00e63eed0c86c1522bad21258
Jérémy Nunes <jeremy.nunes@ynov.com>
2018-12-06 16:24:53 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from "@angular/common/http";
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import { FormControl } from "@angular/forms";
+import { MoviePage } from "../movie/movie";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  searchApi: string = '';
+
+  private movies = [];
+  private term = new FormControl();
+
+  constructor(public navCtrl: NavController, private http: HttpClient) {
 
   }
+
+  ngOnInit() {
+    this.term.valueChanges
+    .debounceTime(500)
+    .distinctUntilChanged()
+    .subscribe(searchTerm => {
+      this.http.get(`http://www.omdbapi.com/?apikey=76b9cca4&s=${searchTerm}`)
+      .subscribe((data: Movie) => {
+        console.log(data);
+        this.movies = data.Search;
+      }).closed
+    });
+  }
+
+  // detailsMovies (movie) {
+  //   this.navCtrl.push(MoviePage, {
+  //     id: movie.imdbID
+  //   });
+  // }
+
 
 }
